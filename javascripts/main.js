3a7da4af01fc3ac353d351fb48c92c16a2dc471d
Aniruddha Nath <aniruddhanath1@gmail.com>
2015-10-02 00:50:01 +0530

    updated with demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+    var data = {
+        canvas: 'canvas',
+        image: 'image',
+        size: 8,
+        scale: 6,
+    };
+
+    Greetings.create(data);
+}
